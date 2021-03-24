ace792fcc33da0eb2df42a83a67502e734f4f2f4
hujxhed <hujxhed@foxmail.com>
2021-03-24 19:46:11 +0800

    modify

--- app/src/main/java/com/bodtec/libs/dahuaplayer/demo/MainAct.kt
+++ app/src/main/java/com/bodtec/libs/dahuaplayer/demo/MainAct.kt
@@ -7,7 +7,7 @@ import kotlinx.android.synthetic.main.act_main.*
 
 class MainAct : AppCompatActivity() {
 
-    private val mToken = "S4NbecfYB1CCT8JFQHEM7P_Y5524jUq1yNQtu+FruA87jL8ok9J5GP2mfM3ngXouKgri3lHI8uiymNDhDYI6AMocMh50p+/tRLJJID1McGgRcXlfKI9ZPSFWTqv9h3volnuhHVob/vpB45bPxlX0zIV"
+    private val mToken = "S4NbecfYB1CCT8JFQHEM7P_Y5524jUq1yNQtu+FruA87jL8ok9J5GP2mfM3ngXouKgri3lHI8uiymNDhDYI6AMoE/fZ1fCyXeeFHhuT3EEMVU+bkvcBzvmQ1vQ1j5isGefuhHVob/vpB45bPxlX0zIV"
     private val mSnCode = "33010662001320101725"
 
     override fun onCreate(savedInstanceState: Bundle?) {
